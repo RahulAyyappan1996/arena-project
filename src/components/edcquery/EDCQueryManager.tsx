80d53819dfdb74a2644e3ed4bce783bedbed50f7
Rahul Ayyappan <68462175+RahulAyyappan1996@users.noreply.github.com>
2026-04-07 17:55:03 +0100

    Create main EDCQueryManager container

---
+++ src/components/edcquery/EDCQueryManager.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EDCQueryManager: React.FC = () => {
+    return (
+        <div>
+            <h1>EDC Query Manager</h1>
+            {/* Implementation goes here */}
+        </div>
+    );
+};
+
+export default EDCQueryManager;
